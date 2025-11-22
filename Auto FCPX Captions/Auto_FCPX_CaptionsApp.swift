bc0b9b05b4a3087c60fcc89909373c7fe5acc7cc
Jamin Zhang <mjz.ovo@gmail.com>
2025-11-22 21:13:25 +0800

    Initial Commit

---
+++ Auto FCPX Captions/Auto_FCPX_CaptionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Auto_FCPX_CaptionsApp.swift
+//  Auto FCPX Captions
+//
+//  Created by Chokamin on 2025/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Auto_FCPX_CaptionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
